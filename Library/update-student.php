815c30b6ca9c25a362b0fc52bcd6a4b30ceb909f
Abiralupreti <44079194+Abiralupreti@users.noreply.github.com>
2020-08-05 22:33:32 -0500

    Final Update

--- Library/update-student.php
+++ Library/update-student.php
@@ -2,13 +2,14 @@
 Santosh Pradhan
 CSCE 3444
 -->
+
 <!DOCTYPE html>
 <html>
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    
     <title>UNT LIBRARY | UPDATE
 STUDENT</title>
 
@@ -18,16 +19,15 @@ STUDENT</title>
     <link rel="stylesheet" href="./css/style.css">
 	<!-- jQuery CDN -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>  
-
+	
 </head>
 
 <?php
-session_start(); //creates a session or resumes the current one based on a session identifier
-if(!isset($_SESSION['user_id']) || $_SESSION['role'] != 'admin' ) //checks if session variable have user_id and role equal admin
-{
-	header('Location: login.php'); //If above is false then redirect to login page 
-}
-require_once("db-connection.php"); // include db connection helper in this php file
+	session_start();
+	if(!isset($_SESSION['user_id']) || $_SESSION['role'] != 'admin' ) {
+	   header('Location: login.php');
+	}
+	require_once("db-connection.php");
 ?>
 
 <body>
@@ -116,13 +116,13 @@ require_once("db-connection.php"); // include db connection helper in this php f
 										  Please provide a valid Search By.
 										</div>
 									  </div>
-
+									  
 									  <div class="col-md-3 mb-3 mr-auto ml-auto">
 										<button id="searchbtn" class="btn-primary form-control" type="button">SEARCH</button>
 									  </div>
 									</div>
 								</form>
-
+											  
 								<table style="display:none" id="searchTable" class="table table-responsive-sm table-bordered">
 								  <thead>
 									<tr>
@@ -134,7 +134,7 @@ require_once("db-connection.php"); // include db connection helper in this php f
 									</tr>
 								  </thead>
 								  <tbody>
-
+									
 								  </tbody>
 								</table>
 							</div>
@@ -144,91 +144,87 @@ require_once("db-connection.php"); // include db connection helper in this php f
 			</div>
 		</div>
 		<footer class="page-footer" style=" background-color: rgba(245, 245, 245, 0.5); ">
-		  <div class="footer-copyright text-center py-4"><h6>Contact : abc@gmail.com &nbsp;&nbsp; Phone : 999100011</h6></div>
+		  <div class="footer-copyright text-center py-4"><h6>Email : sahilpatel@gmail.com &nbsp;&nbsp; Phone : +1 (912) 384 - 1234 &nbsp;&nbsp; Address : 1234 Belt Line road, Dallas, TX, 75250  </h6></div>
 		</footer>		
 	</div>
-
+	
     <!-- Popper.JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
     <!-- Bootstrap JS -->
-
+	
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-
-<script type="text/javascript"> 
-//this method is used add new row in searchTable table
-function addtoTable(id, firstName, lastName, phoneNumber) {
-	$("#searchTable").show();
-	// new row created
-	var markup = "<tr><td class=\"studentID\">" + id + "</td><td>" + firstName + "</td><td>" + lastName + "</td><td>" + phoneNumber + "</td><td><button class=\"action-button\" type=\"button\"><img src=\"./image/edit.png\" width=\"20\" height=\"20\"></button></td></tr>";
-	$("table tbody").append(markup); // new row appended 
-}
-
-(function() {
-	'use strict';
-	window.addEventListener('load', function() { //window on load function
-		//get table body using jquery
-		var tbody = $("#searchTable tbody");
-
-		if (tbody.children().length == 0) { //checks if Table have any row or not
-			$("#searchTable").hide(); //if Table dont have any row then hide table
+    
+    <script type="text/javascript">
+		function addtoTable(id, firstName, lastName, phoneNumber) {
+			$("#searchTable").show();
+			var markup = "<tr><td class=\"studentID\">" + id + "</td><td>" + firstName + "</td><td>" + lastName + "</td><td>" + phoneNumber + "</td><td><button class=\"action-button\" type=\"button\"><img src=\"./image/edit.png\" width=\"20\" height=\"20\"></button></td></tr>";
+			$("table tbody").append(markup);
 		}
+	 
+		(function() {
+			'use strict';
+			window.addEventListener('load', function() {
+				// Fetch all the forms we want to apply custom Bootstrap validation styles to
+				var tbody = $("#searchTable tbody");
 
-		$(".action-button").click(function() { //table row action button click listner
-			var $row = $(this).closest('tr')
-				var $studentID = $row.find(".studentID").text(); //gets student ID from the clicked row
+				if (tbody.children().length == 0) {
+					$("#searchTable").hide();
+				}
+				
+				$(".action-button").click(function() {
+					var $row = $(this).closest('tr')
+					var $studentID = $row.find(".studentID").text();
+					
+					document.location.href = "add-student.php?studentID=" + $studentID;    	    
+				});
+				var form = document.getElementById('searchForm');
+				var button = document.getElementById('searchbtn');
+				// Loop over them and prevent submission
+				button.addEventListener('click', function(event) {
+					if (form.checkValidity() === false) {
+						event.preventDefault();
+						event.stopPropagation();
+						form.classList.add('was-validated');
+					} else{
+						form.submit();
+					} 
+				}, false);
+			}, false);
+		})();
 
-			document.location.href = "add-student.php?studentID=" + $studentID; // redirect to add-student form with student ID   	    
-		});
-		var form = document.getElementById('searchForm');
-		var button = document.getElementById('searchbtn');
-
-		button.addEventListener('click', function(event) { //button click event listner
-			if (form.checkValidity() === false) { //checks if form is valid
-				event.preventDefault(); // if not valid then prevent to submit
-				event.stopPropagation();
-				form.classList.add('was-validated');
-			} else{
-				form.submit(); //if valid then form submit
+		$(document).ready(function () {
+			$('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
+        });
+    </script>
+	
+	<?php   
+		if($_POST){  
+			$searchValue = $_POST['searchValue']; 
+			$searchBy = $_POST['searchBy'];				
+			
+			$sql ="";
+			
+			if($searchBy == 'Last Name') {
+				$sql ="SELECT * FROM `users` WHERE last_name LIKE '%$searchValue%' and role = 'student'";
+			} else if($searchBy == 'Student ID') {
+				$sql ="SELECT * FROM `users` WHERE user_id LIKE '%$searchValue%' and role = 'student'";
+			}
+			
+			$result = $conn->query($sql);
+			if ($result->num_rows > 0) {
+				while($row = $result->fetch_assoc()) {
+					$user_id = $row["user_id"];
+					$last_name = $row["last_name"];
+					$first_name = $row["first_name"];
+					$phone_number = $row["phone_number"];
+					
+					echo "<script>  addtoTable('$user_id', '$first_name', '$last_name', '$phone_number'); </script>" ;
+				}
 			} 
-		}, false);
-	}, false);
-})();
-
-$(document).ready(function () {
-	$('#sidebarCollapse').on('click', function () { //sidebar collapse button event listner
-		$('#sidebar').toggleClass('active'); // sidebar div class value toggle
-	});
-});
-</script>
-
-<?php   
-if($_POST){  // checks if there is post request
-	$searchValue = $_POST['searchValue']; // get searchValue from post request
-	$searchBy = $_POST['searchBy'];	// get searchBy from post request
-
-	$sql ="";
-
-	if($searchBy == 'Last Name') { //if searchBy Last Name
-		$sql ="SELECT * FROM  `users` WHERE last_name LIKE '%$searchValue%' and role = 'student'";
-	} else if($searchBy == 'Student ID') { //else if searchBy Student ID
-		$sql ="SELECT * FROM `users` WHERE user_id LIKE '%$searchValue%' and role = 'student'";
-	}
-
-
-	$result = $conn->query($sql); //execute query
-	if ($result -> num_rows > 0) { //if result row gt than 0
-		while($row = $result->fetch_assoc()) {
-			// get user_id, last_name, first_name, phone_number from row
-			$user_id = $row["user_id"];
-			$last_name = $row["last_name"];
-			$first_name = $row["first_name"];
-			$phone_number = $row["phone_number"];
-			// add user_id, last_name, first_name, phone_number in html table
-			echo "<script>  addtoTable('$user_id', '$first_name', '$last_name', '$phone_number'); </script>" ;
-		}
-	} 
-}  
-?> 
+		}  
+	?> 
 </body>
 
 </html>
